e0dbf2bf5e802c9c3b6ae056dedf46534bd3f129
samuellin <linyusensamuel@gmail.com>
2020-11-12 15:41:33 -0500

    feat: analyze the data distributions

---
+++ analysis/hist_for_similarity.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from lib import path_lib
+
+# load the similarity data
+similarity_csv_path = path_lib.get_relative_file_path('runtime', 'result_csv', 'linkedin_similarity.csv')
+similarity_csv = pd.read_csv(similarity_csv_path)
+
+similarities = list(similarity_csv['similarity'])
+competitor_similarities = list(similarity_csv[similarity_csv['is_competitor'] == 'competitor']['similarity'])
+
+print(f'\n\ncount of all company pairs: {len(similarities)}')
+print(f'mean similarity for all company pairs: {np.mean(similarities)}')
+print(f'std similarity for all company pairs: {np.std(similarities)}')
+print(f'max similarity for all company pairs: {np.max(similarities)}')
+print(f'min similarity for all company pairs: {np.min(similarities)}')
+
+print(f'\ncount of competitor relationship: {len(competitor_similarities)}')
+print(f'mean similarity for competitors: {np.mean(competitor_similarities)}')
+print(f'std similarity for competitors: {np.std(competitor_similarities)}')
+print(f'max similarity for competitors: {np.max(competitor_similarities)}')
+print(f'min similarity for competitors: {np.min(competitor_similarities)}')
+
+plt.figure(figsize=(14, 8))
+plt.subplot(211)
+plt.hist(similarities, bins=30, edgecolor='white')
+plt.title('histogram for similarity of all company pairs', fontsize=22)
+plt.xlabel('similarity', fontsize=16)
+plt.ylabel('count of company pairs', fontsize=16)
+plt.xticks(list(np.linspace(0.1, 1., 10)), fontsize=14)
+plt.xlim(0.1, 1.)
+# plt.savefig(
+#     path_lib.get_relative_file_path('runtime', 'analysis', 'figures', 'hist_for_similarity_for_all_company_pairs.png'),
+#     dpi=300)
+# plt.show()
+# plt.close()
+#
+# plt.figure(figsize=(14, 8))
+plt.subplot(212)
+plt.hist(competitor_similarities, bins=30, edgecolor='white')
+plt.title('histogram for similarity of the competitors', fontsize=22)
+plt.xlabel('similarity', fontsize=16)
+plt.ylabel('count of competitor pairs', fontsize=16)
+plt.xticks(list(np.linspace(0.1, 1., 10)), fontsize=14)
+plt.xlim(0.1, 1.)
+plt.subplots_adjust(hspace=0.45)
+plt.savefig(
+    # path_lib.get_relative_file_path('runtime', 'analysis', 'figures', 'hist_for_similarity_for_competitors.png'),
+    path_lib.get_relative_file_path('runtime', 'analysis', 'figures', 'hist_for_similarity.png'),
+    dpi=300)
+plt.show()
+plt.close()
+
+print('\ndone')
+
+"""
+count of all company pairs: 30112680
+mean similarity for all company pairs: 0.47679189131293215
+std similarity for all company pairs: 0.11996763414226905
+max similarity for all company pairs: 1.0
+min similarity for all company pairs: 0.15235062352614026
+
+count of competitor relationship: 15731
+mean similarity for competitors: 0.5935613760053964
+std similarity for competitors: 0.11671970039768058
+max similarity for competitors: 0.9044875194205836
+min similarity for competitors: 0.20937702813234335
+"""
---
+++ analysis/statistic_pub_company.py
@@ -0,0 +1,36 @@
+from lib import path_lib
+
+# load the json data
+json_path = path_lib.get_relative_file_path('runtime', 'competitor_linkedin_dict_format_v3.json')
+tmp = path_lib.load_json(json_path)
+d_linkedin_name_2_linkedin_val = tmp['d_linkedin_name_2_linkedin_val']
+d_min_linkedin_name_max_linkedin_name = tmp['d_min_linkedin_name_max_linkedin_name']
+
+# for remove duplicated statistics
+d_name_2_has_statistic = {}
+pub_num = 0
+
+# traverse all the companies to statistic the public companies
+name_pairs = list(d_min_linkedin_name_max_linkedin_name.keys())
+for min_name_max_name in name_pairs:
+    name_1, name_2 = min_name_max_name.split('____')
+
+    if name_1 not in d_name_2_has_statistic:
+        d_name_2_has_statistic[name_1] = True
+
+        linkedin_val = d_linkedin_name_2_linkedin_val[name_1]
+        if 'main' in linkedin_val and 'company_type' in linkedin_val['main'] and \
+                linkedin_val['main']['company_type'].lower() == 'public company':
+            pub_num += 1
+
+    if name_2 not in d_name_2_has_statistic:
+        d_name_2_has_statistic[name_2] = True
+
+        linkedin_val = d_linkedin_name_2_linkedin_val[name_2]
+        if 'main' in linkedin_val and 'company_type' in linkedin_val['main'] and \
+                linkedin_val['main']['company_type'].lower() == 'public company':
+            pub_num += 1
+
+print(f'count of public companies: {pub_num}')
+
+# count of public companies: 1981
